18d7810cbcbd9bd7d12f92bf49c90087a0bc0d18
Jia Hao <jiahaog@gmail.com>
2016-02-08 00:34:20 +0800

    Update 2016-02-07T16:34:17.886Z

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBox from './searchBox';
-
-ReactDOM.render(
-    <SearchBox/>,
-    document.getElementById('search-box-container')
-);
