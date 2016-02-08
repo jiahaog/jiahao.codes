4d314584f0c97146303604d736518d1abacc2bba
Jia Hao <jiahaog@gmail.com>
2016-02-08 22:19:47 +0800

    Update education and contact with links to json

--- src/js/articles/contactDOM.js
+++ src/js/articles/contactDOM.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+const resume = require('./../../../data/resume');
+
+const basics = resume.basics;
+const {email} = basics;
+
 function contactDOM() {
     return <section className="section">
         <div className="container">
             <h1 className="title">Contact</h1>
             <h2 className="subtitle">
-                <a href="mailto:jiahao@example.com">jiahao@example.com</a>
+                <a href={`mailto:${email}`}>{email}</a>
             </h2>
         </div>
     </section>
--- src/js/articles/educationDOM.js
+++ src/js/articles/educationDOM.js
@@ -1,13 +1,83 @@
 import React from 'react';
 
-function educationDOM() {
+const resume = require('./../../../data/resume');
+const university = resume.education[0];
+const preUniversity = resume.education[1];
+
+function getYear(dateString) {
+    return new Date(dateString).getUTCFullYear();
+}
+function getYears(startDateString, endDateString) {
+    return {
+        startYear: getYear(startDateString),
+        endYear: getYear(endDateString)
+    }
+}
+
+function universityDOM() {
+    const {institution, area, studyType, track, startDate, endDate, courses, highlights} = university;
+    const {startYear, endYear} = getYears(startDate, endDate);
     return <section className="section">
         <div className="container">
-            <h1 className="title">Education</h1>
-            <h2 className="subtitle">
-                Singapore University of Technology and Design
-            </h2>
+            <div className="columns">
+                <div className="column">
+
+                    <h2 className="title is-4">
+                        {institution}
+                    </h2>
+                    <h2 className="subtitle is-6">
+                        University
+                    </h2>
+                </div>
+                <div className="column is-2">
+                    {startYear} – {endYear}
+                </div>
+            </div>
+            <div className="content">
+                <ul>
+                    <li>{studyType}</li>
+                    <li>{area}</li>
+                    <li>{track} Track</li>
+                    <li>{highlights[0]}</li>
+                </ul>
+            </div>
         </div>
+    </section>
+}
+
+function preUniversityDOM() {
+    const {institution, area, studyType, track, startDate, endDate, courses, highlights} = preUniversity;
+    const {startYear, endYear} = getYears(startDate, endDate);
+    return <section className="section">
+        <div className="container">
+            <div className="columns">
+                <div className="column">
+
+                    <h2 className="title is-4">
+                        {institution}
+                    </h2>
+                    <h2 className="subtitle is-6">
+                        Secondary School & Junior College
+                    </h2>
+                </div>
+                <div className="column is-2">
+                    {startYear} – {endYear}
+                </div>
+            </div>
+            <div className="content">
+                <ul>
+                    <li>{studyType}</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+}
+
+function educationDOM() {
+    return <section className="section">
+            <h1 className="title">Education</h1>
+            {universityDOM()}
+            {preUniversityDOM()}
     </section>
 }
 
