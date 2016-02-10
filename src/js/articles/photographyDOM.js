35d8872a96aaa14f7cbb89ed3b4b05b24118f678
Jia Hao <jiahaog@gmail.com>
2016-02-10 23:57:56 +0800

    Use json for photography, add basic sizing

--- src/js/articles/photographyDOM.js
+++ src/js/articles/photographyDOM.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const IMAGES = ['alp', 'ryb', 'vietnam'];
+const resume = require('./../../../data/resume');
 
-function imageNode(imageName, altText) {
-    return <div className="is-centered">
-        <img className="photo-showcase" src={`img/${imageName}.jpg`} alt={altText}
-             srcSet={`img/${imageName}-480px.jpg 480w, img/${imageName}-960px.jpg 960w, img/${imageName}-1440px.jpg 1440w, img/${imageName}-1920px.jpg 1920w`}
-             sizes="(max-width: 480px) 100vw, (max-width: 900px) 100vw, 700px"/>
+const photographyProjects = resume.photography;
+
+function imageNode(details) {
+    const {name, website, imageName} = details;
+    return <div key={`photography-${imageName}`} className="jh-container is-centered">
+        <a target="_blank" href={website}>
+            <img className="photo-showcase" src={`img/${imageName}.jpg`} alt={name}
+                 srcSet={`img/${imageName}-480px.jpg 480w, img/${imageName}-960px.jpg 960w, img/${imageName}-1440px.jpg 1440w, img/${imageName}-1920px.jpg 1920w`}
+                 sizes="(max-width: 480px) 100vw, (max-width: 900px) 100vw, 700px"/>
+        </a>
     </div>
 }
 
@@ -19,8 +24,8 @@ function photographyDOM() {
             </h2>
         </div>
 
-        {IMAGES.map(fileName => {
-            return imageNode(fileName);
+        {photographyProjects.map(details => {
+            return imageNode(details);
         })}
     </section>
 }
