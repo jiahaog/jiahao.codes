fa572b6116aea18a6608be638f3b46b9939dbbdb
Jia Hao <jiahaog@gmail.com>
2016-02-12 13:11:13 +0800

    Add caption for image

--- src/js/articles/photographyDOM.js
+++ src/js/articles/photographyDOM.js
@@ -8,9 +8,12 @@ function imageNode(details) {
     const {name, website, imageName} = details;
     return <div key={`photography-${imageName}`} className="jh-container is-centered">
         <a target="_blank" href={website}>
-            <img className="photo-showcase" src={`img/${imageName}.jpg`} alt={name}
-                 srcSet={`img/${imageName}-480px.jpg 480w, img/${imageName}-960px.jpg 960w, img/${imageName}-1440px.jpg 1440w, img/${imageName}-1920px.jpg 1920w`}
-                 sizes="(max-width: 480px) 100vw, (max-width: 900px) 100vw, 700px"/>
+            <div className="photo-showcase-container">
+                <img className="photo-showcase-img" src={`img/${imageName}.jpg`} alt={name}
+                     srcSet={`img/${imageName}-480px.jpg 480w, img/${imageName}-960px.jpg 960w, img/${imageName}-1440px.jpg 1440w, img/${imageName}-1920px.jpg 1920w`}
+                     sizes="(max-width: 480px) 100vw, (max-width: 900px) 70vw, 700px"/>
+                <span className="photo-caption title">{name}</span>
+            </div>
         </a>
     </div>
 }
