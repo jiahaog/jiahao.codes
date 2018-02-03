601a40a5e1255c49e1c136205ffe93c915401d68
Goh Jia Hao <jiahaog@gmail.com>
2018-02-03 21:57:56 +0800

    Use stateless components

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,23 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-import profilePic from './profile-pic.jpg'
+import profilePic from './profile-pic.jpg';
 
-class Bio extends React.Component {
-  render() {
-    return (
-      <p>
-        <img
-          src={profilePic}
-          alt={`Kyle Mathews`}
-        />
-        Written by <strong>Kyle Mathews</strong> who lives and works in San
-        Francisco building useful things.{' '}
-        <a href="https://twitter.com/kylemathews">
-          You should follow him on Twitter
-        </a>
-      </p>
-    )
-  }
+export default function Bio() {
+  return (
+    <p>
+      <img src={profilePic} alt={`Kyle Mathews`} />
+      Written by <strong>Kyle Mathews</strong> who lives and works in San
+      Francisco building useful things.{' '}
+      <a href="https://twitter.com/kylemathews">
+        You should follow him on Twitter
+      </a>
+    </p>
+  );
 }
-
-export default Bio
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,41 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+import React from 'react';
+import Link from 'gatsby-link';
+import get from 'lodash/get';
+import Helmet from 'react-helmet';
 
-import Bio from '../components/Bio'
+import Bio from '../components/Bio';
 
-class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+export default function BlogIndex(props) {
+  const siteTitle = get(props, 'data.site.siteMetadata.title');
+  const posts = get(props, 'data.allMarkdownRemark.edges');
 
-    return (
-      <div>
-        <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
-        <Bio />
-        {posts.map(post => {
-          if (post.node.path !== '/404/') {
-            const title = get(post, 'node.frontmatter.title') || post.node.path
-            return (
-              <div key={post.node.frontmatter.path}>
-                <h3>
-                  <Link to={post.node.frontmatter.path} >
-                    {post.node.frontmatter.title}
-                  </Link>
-                </h3>
-                <small>{post.node.frontmatter.date}</small>
-                <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-              </div>
-            )
-          }
-        })}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Helmet title={get(props, 'data.site.siteMetadata.title')} />
+      <Bio />
+      {posts.map(post => {
+        if (post.node.path !== '/404/') {
+          const title = get(post, 'node.frontmatter.title') || post.node.path;
+          return (
+            <div key={post.node.frontmatter.path}>
+              <h3>
+                <Link to={post.node.frontmatter.path}>
+                  {post.node.frontmatter.title}
+                </Link>
+              </h3>
+              <small>{post.node.frontmatter.date}</small>
+              <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 }
 
 BlogIndex.propTypes = {
   route: React.PropTypes.object,
-}
-
-export default BlogIndex
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -63,4 +59,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
