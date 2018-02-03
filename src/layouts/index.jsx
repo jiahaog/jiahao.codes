2487d3af15d27ca57616d393a28ec85ded0ee752
Goh Jia Hao <jiahaog@gmail.com>
2018-02-03 22:41:07 +0800

    Fix eslint for layouts

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,36 +1,32 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-class Template extends React.Component {
-  render() {
-    const { location, children } = this.props;
-    let header;
-    if (location.pathname === '/') {
-      header = (
-        <h1>
-          <Link to="/">Gatsby Starter Blog</Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3>
-          <Link to="/">Gatsby Starter Blog</Link>
-        </h3>
-      );
-    }
-    return (
-      <div>
-        {header}
-        {children()}
-      </div>
+export default function Template({ location, children }) {
+  let header;
+  if (location.pathname === '/') {
+    header = (
+      <h1>
+        <Link to="/">Gatsby Starter Blog</Link>
+      </h1>
+    );
+  } else {
+    header = (
+      <h3>
+        <Link to="/">Gatsby Starter Blog</Link>
+      </h3>
     );
   }
+  return (
+    <div>
+      {header}
+      {children()}
+    </div>
+  );
 }
 
 Template.propTypes = {
-  children: React.PropTypes.func,
-  location: React.PropTypes.object,
-  route: React.PropTypes.object,
+  children: React.PropTypes.func.isRequired,
+  location: React.PropTypes.shapeOf({
+    pathname: React.PropTypes.string.isRequired,
+  }).isRequired,
 };
-
-export default Template;
