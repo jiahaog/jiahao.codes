19e65bdcd1c2d5c7526544f02f19d031534624b3
Goh Jia Hao <jiahaog@gmail.com>
2018-02-04 11:50:23 +0800

    Replace bio with template

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,13 +4,9 @@ import profilePic from './profile-pic.jpg';
 
 export default function Bio() {
   return (
-    <p>
-      <img src={profilePic} alt="Kyle Mathews" />
-      Written by <strong>Kyle Mathews</strong> who lives and works in San
-      Francisco building useful things.{' '}
-      <a href="https://twitter.com/kylemathews">
-        You should follow him on Twitter
-      </a>
-    </p>
+    <div>
+      <p>INTRODUCTION HERE</p>
+      <p>I write software blah blah</p>
+    </div>
   );
 }
