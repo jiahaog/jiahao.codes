85b696ae9d666147b637263b60e0ad50f6dd3db0
Goh Jia Hao <jiahaog@gmail.com>
2018-02-05 01:21:18 +0800

    Styled components to style index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-next',
+    'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- src/pages/IndexContent.jsx
+++ src/pages/IndexContent.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
 
 import Bio from '../components/Bio';
+
+const PostLink = styled(Link)`
+  display: inline-block;
+  width: 100%;
+  background-image: none;
+  border-bottom: 1px solid #eee;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  &:hover {
+    background-color: #f8f8f7;
+  }
+`;
+
+const PostTitle = styled.h3`
+  margin-top: 1rem;
+`;
 
 export default function IndexContent({ posts }) {
   return (
     <div>
       <Bio />
-      {posts.map(({ title, path, date, excerpt }) => (
-        <div key={path}>
-          <h3>
-            <Link to={path}>{title}</Link>
-          </h3>
-          <small>{date}</small>
-          <p dangerouslySetInnerHTML={{ __html: excerpt }} />
-        </div>
-      ))}
+      <div>
+        {posts.map(({ title, path, date, excerpt }) => (
+          <PostLink key={path} to={path}>
+            <PostTitle>{title}</PostTitle>
+            <code>
+              <small>{date}</small>
+            </code>
+            <p dangerouslySetInnerHTML={{ __html: excerpt }} />
+          </PostLink>
+        ))}
+      </div>
     </div>
   );
 }
