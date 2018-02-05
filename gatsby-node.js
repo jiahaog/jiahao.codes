84066054dbb2c7fd2e84b2cf0c27562d62f12945
Goh Jia Hao <jiahaog@gmail.com>
2018-02-06 02:07:34 +0800

    Rename templates/index.jsx

--- gatsby-node.js
+++ gatsby-node.js
@@ -45,6 +45,6 @@ const createPages = (
   });
 
 exports.createPages = async (props) => {
-  await createPages(props, '/src/pages/blog/', './src/templates/index.jsx');
+  await createPages(props, '/src/pages/blog/', './src/templates/Post.jsx');
   await createPages(props, '/src/pages/about/', './src/templates/About.jsx');
 };
