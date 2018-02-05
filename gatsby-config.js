28df90bcde4fb22c191f0af603778b6c5dc6dd01
Goh Jia Hao <jiahaog@gmail.com>
2018-02-06 02:13:53 +0800

    Add GA id

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        // trackingId: 'ADD YOUR TRACKING ID HERE',
+        trackingId: 'UA-66102276-1',
       },
     },
     'gatsby-plugin-offline',
