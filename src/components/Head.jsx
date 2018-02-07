954d4f59eba588d0e19f5ad16074533fc5844651
Goh Jia Hao <jiahaog@gmail.com>
2018-02-07 17:14:02 +0800

    Add favicons

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet';
 import { site as sitePropType } from '../proptypes';
 
 // TODO: find a better image
-const siteCoverImage = require('../../static/favicon.ico');
+const siteCoverImage = require('../../static/favicons/favicon.ico');
 
 const buildStructuredData = ({ siteUrl, siteTitle }) =>
   JSON.stringify([
@@ -49,6 +49,34 @@ function HeadComponent({
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
     </Helmet>
   );
 }
