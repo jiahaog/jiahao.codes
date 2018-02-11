efd6ce6a054b95839a8dae7be20280bad8b7b6f0
Goh Jia Hao <jiahaog@gmail.com>
2018-02-11 19:35:15 +0800

    Remove coverage from package.json

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/components/svgs/*'],
   moduleNameMapper: {
     '\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
