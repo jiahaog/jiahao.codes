86577b0e08dde488aa957a3fb462902599f471ee
Goh Jia Hao <jiahaog@gmail.com>
2018-02-11 18:22:38 +0800

    Add test for layout template and shift mock directory

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/components/svgs/*'],
   moduleNameMapper: {
     '\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+      '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/test/__mocks__/styleMock.js',
   },
   testPathIgnorePatterns: ['/.cache/'],
   setupFiles: ['./test/setup.js'],
