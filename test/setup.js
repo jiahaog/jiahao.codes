2b7ab7aa0a78f30f7cd04423e17cd3e72907a3e2
Goh Jia Hao <jiahaog@gmail.com>
2018-02-11 18:18:13 +0800

    Add snapshot test for fragments

--- test/setup.js
+++ test/setup.js
@@ -2,3 +2,6 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// Setup the global graphql tagged template literals
+global.graphql = (args) => args;
